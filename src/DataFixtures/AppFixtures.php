4a3bf964e490df442bdf477215360845d14d7ad1
Prechh <adamadrien21@gmail.com>
2023-03-06 15:39:39 +0100

    mise en place fixtures de l'entitée User et hachage de MDP

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,38 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Generator;
+use Faker\Factory;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 1; $i < 10; $i++) {
+            $user = new User();
+            $user->setEmail($this->faker->email())
+                ->setRoles(['ROLE_USER'])
+                ->setPlainPassword('password')
+                ->setName($this->faker->name())
+                ->setFirstname($this->faker->firstName())
+                ->setAddress($this->faker->address())
+                ->setPostalcode(mt_rand(01000, 98000))
+                ->setCity($this->faker->city())
+                ->setPhonesNumber(mt_rand(01000, 98000))
+                ->setSiret(mt_rand(01000, 98000));
+
+
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
